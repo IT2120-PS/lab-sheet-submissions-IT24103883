e44b7eb51eb0fb4e8079408eb5d6ac593f23b134
IT24103883 <IT24103883@my.sliit.lk>
2025-09-16 20:28:32 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24103883_Lab_6.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\dasit\\OneDrive\\Desktop\\IT24103883")
+# Q1
+n <- 50
+p <- 0.85
+cat("X ~ Binomial(50, 0.85)\n")
+1 - pbinom(46, size=n, prob=p)
+
+# Q2
+lambda <- 12
+cat("X = number of calls per hour\n")
+cat("X ~ Poisson(12)\n")
+dpois(15, lambda)
