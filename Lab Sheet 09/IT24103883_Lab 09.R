b351740bc950d2e0d5f01eae0271dcefc256d9cc
IT24103883 <IT24103883@my.sliit.lk>
2025-10-02 02:19:24 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT24103883_Lab 09.R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\DELL\\OneDrive\\Desktop\\IT24103883")
+
+set.seed(123)
+sample_data <- rnorm(25, mean = 45, sd = 2)
+sample_data
+
+t.test(sample_data, mu = 46, alternative = "less", conf.level = 0.95)
+
+
